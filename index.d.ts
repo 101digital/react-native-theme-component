c4216b1cf2f794e0de7e493101faca6968cb24e9
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-07-11 17:21:11 +0700

    udpate

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ export { default as AlertModal } from './src/alert';
 export { default as Button } from './src/button';
 export { default as BottomSheet } from './src/bottom-sheet';
 export { default as InputField } from './src/input-field';
+export { default as InputPhoneNumber } from './src/input-phone-number';
 export {
   ThemeContext,
   ThemeProvider,
