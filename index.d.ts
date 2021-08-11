be3b83259db5560089382d7f5ec6dca8ff215cd1
Tuyen Nguyen <tuyennguyen160296@gmail.com>
2021-08-11 14:47:07 +0700

    update elements

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export { default as Button } from './src/button';
 export { default as BottomSheet } from './src/bottom-sheet';
 export { default as InputField } from './src/input-field';
 export { default as InputPhoneNumber } from './src/input-phone-number';
+export { default as ErrorModal } from './src/error-modal'
 export { default as Image } from './src/image';
 export { InputFieldStyles } from './src/input-field/index';
 export { InputPhoneNumberStyles } from './src/input-phone-number/index';
