bb7a9f29ba98fa6335aaddd5e57941aaff0921a6
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2021-12-17 17:01:32 +0700

    update

--- gitpkg.config.js
+++ gitpkg.config.js
@@ -1,4 +1,4 @@
 module.exports = () => ({
     registry: "https://github.com/101digital/react-native-theme-component.git",
-    getTagName: (pkg) => `v${pkg.version}`,
+    getTagName: (pkg) => `${pkg.version}`,
   });
