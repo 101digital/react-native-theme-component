77dae17a9266e2254dc0b5a9716dd175bfe0ed56
khoi anh <anhkhoi592014@gmail.com>
2022-06-30 19:58:56 +0700

    export ref and raise version

--- index.d.ts
+++ index.d.ts
@@ -23,6 +23,7 @@ export { default as KeyboardSpace } from './src/keyboard-space';
 export { default as OTPField } from './src/otp-field';
 export { default as CountdownTimer } from './src/countdown-timer';
 export { default as Carousel } from './src/carousel';
+export { CarouselRef } from './src/carousel';
 export { showMessage } from 'react-native-flash-message';
 export * from './src/utils/currency-format';
 export * from './src/colors';
